b1da8bbd0381abb8d3cdf4cd86a352d7f88fb24b
Adendorff, D, Miss [24051055@sun.ac.za] <24051055@sun.ac.za>
2023-08-02 11:38:36 +0200

    [#4]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
-public class Main{
+public class Greeter {
+
     public static void main(String [] args){
-        
+        if(args.length != 1){
+            System.out.println("Usage: java Greeter <name>");
+        }else{
+            greeter(args[0]);
+        }
+    }
+
+    public static void greeter(String name){
+        System.out.println("Hello " + name);
     }
 
     public static String GetName(){
