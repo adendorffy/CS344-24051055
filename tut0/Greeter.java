d4a355ed3f54d827d8a1f3ed7c669f7c396e410e
Adendorff, D, Miss [24051055@sun.ac.za] <24051055@sun.ac.za>
2023-08-02 11:45:09 +0200

    [#9]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String [] args){
         
+    }
+
+    public static String GetName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        return name;
     }
 }
